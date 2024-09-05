914c80f6a02cc6349bc5d52ddd8e6627d3d5877e
Utkarsh <utkarsh.singh@hpe.com>
2024-09-05 22:38:53 +0530

    function

--- profit_calculator/profit_calculator.go
+++ profit_calculator/profit_calculator.go
@@ -2,21 +2,29 @@ package main
 
 import "fmt"
 
-func main() {
-	var (
-		revenue  float64
-		expenses float64
-		tax_rate float64
-	)
-	fmt.Print("Total Revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Total Expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&tax_rate)
+var (
+	scan     float64
+	revenue  float64
+	expenses float64
+	tax_rate float64
+)
 
+func userInput(text string) float64 {
+	fmt.Print(text)
+	fmt.Scan(&scan)
+	return scan
+}
+func calculate_ebt_profit_ratio(revenue, expenses, tax_rate float64) (float64, float64, float64) {
 	earning_before_tax := revenue - expenses
 	profit := earning_before_tax * (1 - tax_rate/100)
 	ratio := revenue / profit
+	return earning_before_tax, profit, ratio
+}
+
+func main() {
+	revenue = userInput("Total Revenue: ")
+	expenses = userInput("Total Expenses: ")
+	tax_rate = userInput("Tax Rate: ")
+	earning_before_tax, profit, ratio := calculate_ebt_profit_ratio(revenue, expenses, tax_rate)
 	fmt.Printf("Earning before paying tax is: %.2f ,the net profit is: %.2f and the Ratio is: %.2f", earning_before_tax, profit, ratio)
 }
